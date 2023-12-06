278ba47c93e873d0bf8318bcdea73d89f89701b4
rohitraj94848 <138149999+rohitraj94848@users.noreply.github.com>
2023-12-06 21:50:14 +0530

    Time: 1 ms (60.95%) | Memory: 39.7 MB (13.57%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
@@ -1,21 +1,24 @@
 class Solution {
-  public int totalMoney(int n){
-    int totalMoney = 0;
-    int dailySum = 0, weekSum = 0;
-    for(int day = 1; day<=n ; day++ ){
-      if(day % 7 == 1)
-      {
-        weekSum++;
-        dailySum = weekSum;
-      }
-      else
-      {
-        dailySum++;
-        
-      }
-    totalMoney += dailySum;
-      
+  public int totalMoney(int n) {
+  int totalMoney = 0;
+  int dailySum = 0, weeksCount = 0;
+
+  // Loop through each day from 1 to n
+  for (int day = 1; day <= n; day++) {
+    // Check if it's the first day of a week
+    if (day % 7 == 1) {
+      weeksCount++;  // Increment the week count
+      dailySum = weeksCount;  // Reset the daily sum for a new week
+    } else {
+      dailySum++;  // Increment the daily sum for the current week
     }
-    return totalMoney;
+
+    // Accumulate the daily sum to the total money
+    totalMoney += dailySum;
   }
+
+  // Return the total money after n days
+  return totalMoney;
+}
+
 }
