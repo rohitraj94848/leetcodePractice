21fe38af2a0966849233518d0459a95405bddd7b
rohitraj94848 <138149999+rohitraj94848@users.noreply.github.com>
2024-06-20 18:46:02 +0530

    Time: 4 ms (99.67%) | Memory: 45.3 MB (51.62%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                // Characters at left and right indices don't match
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+    
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false; // It's not a palindrome
+            }
+            left++;
+            right--;
+        }
+        return true; // It's a palindrome
+    }
+}
